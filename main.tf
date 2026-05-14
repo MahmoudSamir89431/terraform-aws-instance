ff5863617f8204cac5d1f271e2e6be011291f756
Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
2026-05-14 11:31:42 +0200

    new files

--- main.tf
+++ main.tf
@@ -1,5 +1,37 @@
+variable "name_prefix" {
+    type        = string
+    description = "The prefix to apply at the begining of names genreted by this module"
+}
+
+variable "tags" {
+    type = map(string)
+    description = "Key/value pairs to pass to AWS as Tags"
+    default     = {}
+  
+}
+
+variable "instance_count" {
+    type        = number
+    description = "The number of instances to launch"
+    default     = 0
+    validation {
+      condition = can(parseint(tostring(var.my_integer), 10))
+      error_message = "The instant count must be whole number."
+    }
+    validation {
+      condition = var.instance_count >=0
+      error_message = "The instant count can't be negative."
+    }
+  
+}
+
 resource "aws_instance" "hello_world" {
-    ami                = data.aws_ami.ubuntu.id
-    subnet_id          = data.aws_subnets.default.ids[0]
-    instance_type       = "t3.micro"
+    count               = var.instance_count
+    ami                 = data.aws_ami.ubuntu.id
+    subnet_id           = var.subnet_id
+    instance_type       = var.instance_type
+
+    tags = merge(var.tags, {
+        Name = "${var.name_prefix}-instance"
+    })
 }
--- output.tf
+++ output.tf
@@ -9,5 +9,9 @@ output "aws_instance_ip" {
 
 output "aws_instance" {
     value = aws_instance.hello_world
+}
+
+output "aws_instance_role" {
+    value = aws_iam_role.main
   
 }
